71fa50764ae54c38e54e70298667955428c1d813
Sam Day <me@samcday.com>
2019-04-03 15:58:12 +0200

    go/hello-world

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,10 @@
+// Package greeting employs a legion of electrons that whizz around a silicon
+// wafer comprised of billions of transistors, utilizing thousands of centuries
+// of human inginuity that may someday lead us to the stars ... to say hello,
+// world!
+package greeting
+
+// Do the thing with the hello. and the world.
+func HelloWorld() string {
+	return "Hello, World!"
+}
