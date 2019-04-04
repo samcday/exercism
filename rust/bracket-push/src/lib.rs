45a340c5d3ddc456d08cf47600d32f44e1e8db84
Sam Day <me@samcday.com>
2019-04-04 16:29:44 +0200

    rust/bracket-push: improve answer based on https://exercism.io/tracks/rust/exercises/bracket-push/solutions/55ba5c922ac14cbe8b5e61e891f506e5

--- rust/bracket-push/src/lib.rs
+++ rust/bracket-push/src/lib.rs
@@ -2,16 +2,11 @@ pub fn brackets_are_balanced(string: &str) -> bool {
     let mut stack = Vec::new();
     for c in string.chars() {
         match c {
-            '(' | '{' | '[' => stack.push(c), // New opening bracket
+            '(' => stack.push(')'),
+            '{' => stack.push('}'),
+            '[' => stack.push(']'),
             ']' | '}' | ')' => {
-                // Closing bracket. Ensure it matches expected.
-                let expected = match stack.pop() {
-                    Some('(') => ')',
-                    Some('[') => ']',
-                    Some('{') => '}',
-                    _ => return false,
-                };
-                if expected != c {
+                if stack.pop() != Some(c) {
                     return false;
                 }
             }
