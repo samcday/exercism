1f3f36c03ed098eb09b62c21c9ca695478dec0dd
Sam Day <me@samcday.com>
2019-04-04 15:50:10 +0200

    rust/gigasecond simplify + const

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,11 +1,8 @@
 use chrono::{DateTime, Duration, Utc};
 
+const ONE_BILLYUN: i64 = 1_000_000_000;
+
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    // Use the checked add and unwrap() it so that we panic. This way users of
-    // our program will know they're executing it in the midst of the heat death
-    // of the universe.
-    start
-        .checked_add_signed(Duration::seconds(1_000_000_000))
-        .unwrap()
+    start + Duration::seconds(ONE_BILLYUN)
 }
