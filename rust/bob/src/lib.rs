cba5b62e6ae964818314fc785e31597207d863cd
Sam Day <me@samcday.com>
2019-04-04 16:10:43 +0200

    rust/bob

---
+++ rust/bob/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn reply(message: &str) -> &str {
+    let message = message.trim();
+
+    if message.len() == 0 {
+        return "Fine. Be that way!";
+    }
+
+    let mut chars = message.chars().rev();
+    let last_char = chars.next().unwrap();
+
+    let mut chars = chars.filter(|c| c.is_alphabetic()).peekable();
+    let is_shouting = chars.peek().is_some() && chars.all(|c| c.is_uppercase());
+
+    match last_char {
+        '?' if is_shouting => "Calm down, I know what I'm doing!",
+        '?' => "Sure.",
+        _ if is_shouting => "Whoa, chill out!",
+        _ => "Whatever.",
+    }
+}
