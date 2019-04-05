783ec5b2fe87a8667238e84a18b65578c1caeafe
Sam Day <me@samcday.com>
2019-04-05 14:36:07 +0200

    rust/paasio

---
+++ rust/paasio/src/lib.rs
@@ -0,0 +1,83 @@
+#![deny(clippy::all, clippy::pedantic)]
+
+use std::io::{Read, Result, Write};
+
+pub struct ReadStats<R> {
+    wrapped: R,
+    bytes: usize,
+    reads: usize,
+}
+
+impl<R: Read> ReadStats<R> {
+    pub fn new(wrapped: R) -> Self {
+        Self {
+            wrapped,
+            bytes: 0,
+            reads: 0,
+        }
+    }
+
+    pub fn get_ref(&self) -> &R {
+        &self.wrapped
+    }
+
+    pub fn bytes_through(&self) -> usize {
+        self.bytes
+    }
+
+    pub fn reads(&self) -> usize {
+        self.reads
+    }
+}
+
+impl<R: Read> Read for ReadStats<R> {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        self.reads += 1;
+        self.wrapped.read(buf).and_then(|bytes| {
+            self.bytes += bytes;
+            Ok(bytes)
+        })
+    }
+}
+
+pub struct WriteStats<W> {
+    wrapped: W,
+    bytes: usize,
+    writes: usize,
+}
+
+impl<W: Write> WriteStats<W> {
+    pub fn new(wrapped: W) -> Self {
+        Self {
+            wrapped,
+            bytes: 0,
+            writes: 0,
+        }
+    }
+
+    pub fn get_ref(&self) -> &W {
+        &self.wrapped
+    }
+
+    pub fn bytes_through(&self) -> usize {
+        self.bytes
+    }
+
+    pub fn writes(&self) -> usize {
+        self.writes
+    }
+}
+
+impl<W: Write> Write for WriteStats<W> {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        self.writes += 1;
+        self.wrapped.write(buf).and_then(|bytes| {
+            self.bytes += bytes;
+            Ok(bytes)
+        })
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        self.wrapped.flush()
+    }
+}
