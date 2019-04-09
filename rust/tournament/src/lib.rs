f03bc09b7fc844589e01737d26dc4a055887a60b
Sam Day <me@samcday.com>
2019-04-09 10:29:40 +0200

    rust/tournament: a bit of code golfing and also improved the performance

--- rust/tournament/src/lib.rs
+++ rust/tournament/src/lib.rs
@@ -1,12 +1,13 @@
 #![deny(clippy::all, clippy::pedantic)]
 
 use std::collections::HashMap;
+use std::fmt::Write;
 
 const HEADER: &str = "Team                           | MP |  W |  D |  L |  P";
 
 #[derive(Default)]
-struct TeamInfo(u16, u16, u16, u16, u16);
-impl std::ops::AddAssign for TeamInfo {
+struct TeamStats(u16, u16, u16, u16, u16);
+impl std::ops::AddAssign for TeamStats {
     fn add_assign(&mut self, o: Self) {
         self.0 += o.0;
         self.1 += o.1;
@@ -17,40 +18,31 @@ impl std::ops::AddAssign for TeamInfo {
 }
 
 pub fn tally(match_results: &str) -> String {
-    let mut teams: HashMap<&str, TeamInfo> = HashMap::new();
-
-    for (line, data) in match_results.lines().enumerate() {
-        let parts = data.split(';').collect::<Vec<&str>>();
-        if parts.len() != 3 {
-            panic!("malformed data on line {}", line);
-        }
-
-        let (team_1, team_2, result) = (parts[0], parts[1], parts[2]);
-        let (team_1_results, team_2_results) = match result {
-            "win" => (TeamInfo(1, 1, 0, 0, 3), TeamInfo(1, 0, 1, 0, 0)),
-            "loss" => (TeamInfo(1, 0, 1, 0, 0), TeamInfo(1, 1, 0, 0, 3)),
-            "draw" => (TeamInfo(1, 0, 0, 1, 1), TeamInfo(1, 0, 0, 1, 1)),
-            _ => Default::default(),
-        };
-
-        *teams.entry(team_1).or_default() += team_1_results;
-        *teams.entry(team_2).or_default() += team_2_results;
-    }
+    let mut teams: HashMap<&str, TeamStats> = HashMap::new();
+    match_results
+        .lines()
+        .map(|data| data.split(';').collect::<Vec<&str>>())
+        .enumerate()
+        .for_each(|(line, match_info)| {
+            assert_eq!(match_info.len(), 3, "Malformed data on line {}", line);
+            let (team_1_results, team_2_results) = match match_info[2] {
+                "win" => (TeamStats(1, 1, 0, 0, 3), TeamStats(1, 0, 1, 0, 0)),
+                "loss" => (TeamStats(1, 0, 1, 0, 0), TeamStats(1, 1, 0, 0, 3)),
+                "draw" => (TeamStats(1, 0, 0, 1, 1), TeamStats(1, 0, 0, 1, 1)),
+                result => panic!("Unexpected match result: {}", result),
+            };
+            *teams.entry(match_info[0]).or_default() += team_1_results;
+            *teams.entry(match_info[1]).or_default() += team_2_results;
+        });
 
     let mut sorted_teams = teams.keys().cloned().collect::<Vec<&str>>();
-    sorted_teams.sort_unstable_by(|team1, team2| {
-        teams[team2].4.cmp(&teams[team1].4).then(team1.cmp(team2))
-    });
+    sorted_teams.sort_unstable_by(|team1, team2| teams[team2].4.cmp(&teams[team1].4).then(team1.cmp(team2)));
 
-    [HEADER.to_string()]
-        .iter()
-        .cloned()
-        .chain(sorted_teams.iter().map(|team_name| {
-            let TeamInfo(team_mp, team_wins, team_losses, team_draws, team_pts) = teams[team_name];
-            format!(
-                "\n{:31}| {:^3}| {:^3}| {:^3}| {:^3}|{:>3}",
-                team_name, team_mp, team_wins, team_draws, team_losses, team_pts
-            )
-        }))
-        .collect::<String>()
+    let mut out = String::with_capacity((1 + teams.len()) * HEADER.len());
+    out.push_str(HEADER);
+    for name in &sorted_teams {
+        let TeamStats(mp, w, l, d, pts) = teams[name];
+        write!(out, "\n{:31}| {:^3}| {:^3}| {:^3}| {:^3}|{:>3}", name, mp, w, d, l, pts).unwrap()
+    }
+    out
 }
