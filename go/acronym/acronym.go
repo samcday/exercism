a45a5dcb79782896b4a0f373ed8e9c88588bcaab
Sam Day <me@samcday.com>
2020-08-24 14:55:16 +0200

    go/acronym: use strings.Builder

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,12 +1,13 @@
 package acronym
 
 import (
+	"strings"
 	"unicode"
 )
 
 // Abbreviate is the exercise solution
 func Abbreviate(s string) string {
-	acronym := ""
+	var acronym strings.Builder
 	firstLetter := true
 
 	for _, r := range s {
@@ -14,10 +15,10 @@ func Abbreviate(s string) string {
 		case unicode.IsSpace(r) || r == '-':
 			firstLetter = true
 		case firstLetter && unicode.IsLetter(r):
-			acronym += string(unicode.ToUpper(r))
+			acronym.WriteRune(unicode.ToUpper(r))
 			firstLetter = false
 		}
 	}
 
-	return acronym
+	return acronym.String()
 }
